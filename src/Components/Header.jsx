3c5b70ccdf9156c7e7f8760db98cd4564d840a4e
huchchas <hsuchchas@gmail.com>
2023-01-03 23:34:10 -0500

    Second commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <nav>
+      <div> LOGO </div>
+      <div>
+        <a href="/personal"> personal </a>
+        <a href="/portfolio"> portfolio </a>
+        <a href="/blog"> blog </a>
+        <a href="/contact"> contact </a>
+        <a href="/resume"> resume </a>
+      </div>
+    </nav>
+  )
+}
